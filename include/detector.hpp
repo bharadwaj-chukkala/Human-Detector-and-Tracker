3761c2db24cd914e126bb31e628d29e3ea19ab63
Shelvin Pauly <spauly@umd.edu>
2022-10-16 21:56:01 -0400

    Pushed detector header file

---
+++ include/detector.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+class detector {
+    std::array<int> drawboundingbox();
+    std::vector<point> applySVM();
+    public:
+    std::array<int> preprocess();
+}
