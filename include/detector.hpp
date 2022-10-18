de8b3d36d593a4112b1d3eed06f779c7b23810e4
Bharadwaj Chukkala <106445479+bharadwaj-chukkala@users.noreply.github.com>
2022-10-18 11:41:17 -0400

    delete include directory for now

--- include/detector.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-class detector {
-    std::array<int> drawboundingbox();
-    std::vector<point> applySVM();
-    public:
-    std::array<int> preprocess();
-}
