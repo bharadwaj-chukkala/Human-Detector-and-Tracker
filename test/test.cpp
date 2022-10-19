754b4762326b6b4336527ea01489b32d24e3b94e
spauly98 <spauly@umd.edu>
2022-10-19 13:31:12 -0400

    Updated test.cpp for the unit testing of ReadData

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
+#include <"../include/ReadData.hpp">
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+// TEST(dummy, should_pass) {
+//   EXPECT_EQ(1, 1);
+// }
+
+DataReader data;
+std::string path = "../test_data/Lenna.png";
+
+TEST(DataReader, read_success) {
+
+  cv::Mat frame;
+  frame = data.ReadFrame(path)
+  EXPECT_FALSE(frame.empty());
 }
