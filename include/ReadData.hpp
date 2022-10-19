cc42d1dd880cc13501a2558c1b8722bd48baa6e7
spauly98 <spauly@umd.edu>
2022-10-19 12:21:32 -0400

    [1.9.2] Added header file for Reading the data (ReadData.hpp)

---
+++ include/ReadData.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file ReadData.hpp
+ * @author Bharadwaj Chukkala (bchukkal@umd.edu),Venkata Sairam Polina (sairamp@umd.edu),Shelvin Pauly (spauly@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <array>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+/**
+ * @brief ReadData class which reads the input frame by frame 
+ * 
+ */
+class ReadData{
+    cv::Mat frame;
+
+    public:
+    /**
+     * @brief Construct a new Read Data object
+     * 
+     */
+    ReadData(cv::Mat);
+
+    /**
+     * @brief Destroy the Read Data object
+     * 
+     */
+
+    ~ReadData();
+    
+    /**
+     * @brief method which reads an image from the path
+     * 
+     * @return cv::Mat image(frame)
+     */
+    //cv::Mat readFrame(std::string path);
+}
