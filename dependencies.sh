f1dba5876115bcc123ac3321f76f5d1d4e0f26c1
spauly98 <spauly@umd.edu>
2022-10-19 20:04:47 -0400

    Changes in travis.yml anf dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,40 +1,13 @@
-sudo apt-get install build-essential checkinstall cmake pkg-config yasm gfortran git
-sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
-## If you are using Ubuntu 14.04
-# sudo apt-get install libtiff4-dev
-## If you are using Ubuntu 16.04
-sudo apt-get install libtiff5-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-sudo apt-get install libxine2-dev libv4l-dev
-sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install libqt4-dev libgtk2.0-dev libtbb-dev
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
-sudo apt-get install libvorbis-dev libxvidcore-dev
-sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install x264 v4l-utils
+sudo apt install -y g++ cmake make git libgtk2.0-dev pkg-config
 
 ### For opencv
 git clone https://github.com/opencv/opencv.git
-cd opencv 
-cd ..
-git clone https://github.com/opencv/opencv_contrib.git
-cd opencv_contrib
-cd ..
 cd opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D INSTALL_C_EXAMPLES=ON \
-      -D WITH_TBB=ON \
-      -D WITH_V4L=ON \
-      -D WITH_QT=ON \
-      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-      -D BUILD_EXAMPLES=ON \
-      -D WITH_CUDA=OFF ..
-
+mkdir build && cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_EXAMPLES=OFF -D BUILD_opencv_apps=OFF -D BUILD_DOCS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j4
 sudo make install
+
 sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
+cd
