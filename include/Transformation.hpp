b09afa6176b433c364f367846ebd2d99cf5a23af
sairampolina <polinavenkatasairam@gmail.com>
2022-10-26 14:53:52 -0400

    [2.5.1]:adding Eigen header

--- include/Transformation.hpp
+++ include/Transformation.hpp
@@ -1,11 +1,13 @@
 
+# include <Eigen/Dense>
+
 class Transformaton{
     Eigen::MatrixXf K;
     Eigen::MatrixXf RT;
     Eigen::MatrixXf P;
     public:
     Transformation();
-    
+
     cv::Point3D doTransform(cv::Point imageCoord){
 
     }
